a104d0bde2a63e5744bf1b11f4f68e8b3e152e65
sidalihmdn <essai@MacBook-Air-de-Essai.local>
2023-06-14 02:20:49 +0200

    - add somme globale variables

--- source.py
+++ source.py
@@ -9,7 +9,10 @@ import time
 import cv2
 
 
-
+IMAGE_SIZE = 200
+IMAGES_NUMBER = 1018
+CLOUD_SIZE = 200
+IMAGE_SIZE_tuple = (IMAGE_SIZE,IMAGE_SIZE)
 
 #this function moves a given particle in a random direction
 def move(X):
@@ -28,9 +31,9 @@ def move(X):
     
 # this function cheks the color of every pixel of a given image
 def mesure(img):
-    z = np.zeros((200,200)).tolist()
-    for i in range(200):
-        for j in range(200):
+    z = np.zeros(IMAGE_SIZE_tuple).tolist()
+    for i in range(IMAGE_SIZE):
+        for j in range(IMAGE_SIZE):
             if (img[i][j] == (255,0,0)).all():
                 z[i][j] = True
             else :
@@ -42,7 +45,7 @@ def mesure(img):
 # this function loads the set of images
 def load():
     imgs = []
-    for i in range(1,1018):
+    for i in range(1,IMAGES_NUMBER):
         # set the names of the files
         if i < 10 :
             name = f'snake_000{i}.png'
@@ -70,8 +73,8 @@ def animate(imgs):
                 break  
     
 def gen_cloud(n_samples):
-    x = np.random.randint(1 , 200 , n_samples)
-    y = np.random.randint(1 , 200 , n_samples)
+    x = np.random.randint(1 , IMAGE_SIZE , n_samples)
+    y = np.random.randint(1 , IMAGE_SIZE , n_samples)
     return [(x[i],y[i]) for i in range(n_samples) ]
 
 
@@ -94,7 +97,7 @@ def particul_filter(x_init , z ):
         p[i] = (w_t[i]*x_t[i][0] , w_t[i]*x_t[i][1])
     
     # resampling
-    for i in range(100):
+    for i in range(CLOUD_SIZE/2):
         # this condition verifies that we know the position of the snake and that the particle isn't on the snake
         if (w_t[i] == 0 and len(snake)):
             # we give to this particle the position of a particle which is on the snake after moving it randomly
@@ -105,16 +108,16 @@ def particul_filter(x_init , z ):
             p[i] = p[i]
         else :
             # creating a random particle
-            p[i] = (np.random.randint(1,200) , np.random.randint(1,200))
+            p[i] = (np.random.randint(1,IMAGE_SIZE) , np.random.randint(1,IMAGE_SIZE))
     return p
 
 def weights(p,z):
     pixel = list(p)
     pixel = [abs(pix) for pix in pixel]
     
-    if (pixel[0] == 0 or pixel[0] >= 200) :
+    if (pixel[0] == 0 or pixel[0] >= IMAGE_SIZE) :
         pixel[0] = 1
-    if (pixel[1] == 0 or pixel[1] >= 200) :
+    if (pixel[1] == 0 or pixel[1] >= IMAGE_SIZE) :
         pixel[1] = 1
 
     #mesurement
@@ -127,7 +130,7 @@ def weights(p,z):
 
 imgs = load()
 Z = mesure(imgs[2])
-X = gen_cloud(200)
+X = gen_cloud(CLOUD_SIZE)
 
 for i in range(500) :
     # adding the particle on the image
